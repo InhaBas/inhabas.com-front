d7540d394caad5cfe2fe324ed95fd973bcbd0d83
Yejin0O0 <yyj11kr@naver.com>
2022-02-10 14:18:48 +0900

    * vue axios 설치 *

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App"
 import router from './routes/index' //router 도입
 import './index.css' // tailwind.css
 // import Vuex from "vuex"
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Particles from "particles.vue3"; // std_or_pro 뒷 배경 애니메이션
 
 const app = createApp(App);
 
 app.use(router)
+app.use(VueAxios, axios)
 app.use(Particles)
 app.mount('#app')
 
