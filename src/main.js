5a8ae93a94df3bc32bd68f27280ad9afbb4d3d13
Yejin0O0 <yyj11kr@naver.com>
2022-02-10 14:09:24 +0900

    * vuex 설치 *

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from "./App"
 import router from './routes/index' //router 도입
 import './index.css' // tailwind.css
+// import Vuex from "vuex"
 import Particles from "particles.vue3"; // std_or_pro 뒷 배경 애니메이션
 
 const app = createApp(App);
