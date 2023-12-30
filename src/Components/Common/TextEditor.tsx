a408a31b5230ce6185b4a7c48f8a98b5703eb7d8
Yejin0O0 <yyj11kr@naver.com>
2023-12-30 15:10:28 +0900

    Feat: Toast UI 사용하여 텍스트 에디터 UI 적용

---
+++ src/Components/Common/TextEditor.tsx
@@ -0,0 +1,50 @@
+// Toast UI Editor
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+// Color Syntax Plugin
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+import "tui-color-picker/dist/tui-color-picker.css";
+
+const colorSyntaxOptions = {
+    preset: [
+        "#333333",
+        "#666666",
+        "#FFFFFF",
+        "#EE2323",
+        "#F89009",
+        "#009A87",
+        "#006DD7",
+        "#8A3DB6",
+        "#333333",
+        "#666666",
+        "#FFFFFF",
+        "#EE2323",
+        "#F89009",
+        "#009A87",
+        "#006DD7",
+        "#8A3DB6",
+    ],
+};
+
+const TextEditor = () => {
+    return (
+        <Editor
+            height="500px"
+            previewStyle="vertical"
+            initialEditType="markdown"
+            toolbarItems={[
+                // 툴바 옵션 설정
+                ["heading", "bold", "italic", "strike"],
+                ["hr", "quote"],
+                ["ul", "ol", "task", "indent", "outdent"],
+                ["table", "image", "link"],
+                ["code", "codeblock"],
+            ]}
+            usageStatistics={false} // 통계 수집 거부
+            plugins={[[colorSyntax, colorSyntaxOptions]]}
+        />
+    );
+};
+
+export default TextEditor;
