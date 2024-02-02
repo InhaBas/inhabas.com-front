db528c87cf9b6d2ce5ea5a46c3374c097510b475
Yejin0O0 <yyj11kr@naver.com>
2024-02-02 11:07:27 +0900

    [Feat/#112] 재학생 회원관리 더보기 페이지 완성

---
+++ src/Components/Page/IBAS/MyInfo/MyManageExistUser.tsx
@@ -0,0 +1,62 @@
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+import { theme } from "../../../../styles/theme";
+
+import { totalUserInfo } from "../../../../Recoil/backState";
+
+import { Container, Div, FlexDiv } from "../../../../styles/assets/Div";
+import Img from "../../../../styles/assets/Img";
+import P from "../../../../styles/assets/P";
+
+import MyUserTable from "../../../Component/IBAS/MyInfo/MyUserTable";
+
+const MyManageExistUser = () => {
+    const navigate = useNavigate();
+    const totalUser = useRecoilValue(totalUserInfo);
+
+    const movePage = (url: string) => {
+        navigate(url);
+    };
+
+    return (
+        <FlexDiv width="100%" $border={`1px solid ${theme.color.grey1}`}>
+            <Container>
+                <Div width="100%" $border="1px solid" $borderColor="border" $margin=" 0 0 20px 0" radius={6}>
+                    <FlexDiv
+                        width=" 100%"
+                        $padding="20px"
+                        $justifycontent="space-between"
+                        $borderB={`1px solid ${theme.color.border}`}
+                    >
+                        <FlexDiv>
+                            <FlexDiv width="20px" height="15px" $margin="0 10px 0 0">
+                                <Img src="/images/user_purple.svg" />
+                            </FlexDiv>
+                            <Div $margin="0 5px 0 0 ">
+                                <P fontWeight={600}>동아리원 현황 </P>
+                            </Div>
+                            <Div>
+                                <P color="red" fontSize="xs" fontWeight={800}>
+                                    ({totalUser})
+                                </P>
+                            </Div>
+                        </FlexDiv>
+                        <FlexDiv $pointer onClick={() => movePage("/staff/member")}>
+                            <P color="textColor" fontSize="xs">
+                                목록으로
+                            </P>
+                        </FlexDiv>
+                    </FlexDiv>
+                    <FlexDiv width="100%" $justifycontent="start" $padding="30px">
+                        <FlexDiv width="100%" $justifycontent="space-around">
+                            <MyUserTable />
+                        </FlexDiv>
+                    </FlexDiv>
+                </Div>
+            </Container>
+        </FlexDiv>
+    );
+};
+
+export default MyManageExistUser;
--- src/Layout/HeaderTitleLayout.tsx
+++ src/Layout/HeaderTitleLayout.tsx
@@ -5,6 +5,7 @@ import BoardRoute from "../Routes/BoardRoute";
 import LectureRoute from "../Routes/LectureRoute";
 import MainRoute from "../Routes/MainRoute";
 
+import MyManageExistUser from "../Components/Page/IBAS/MyInfo/MyManageExistUser";
 import MyManageNewUser from "../Components/Page/IBAS/MyInfo/MyManageNewUser";
 import { FlexDiv } from "../styles/assets/Div";
 
@@ -20,6 +21,7 @@ const HeaderTitlePage = () => {
 
                     <Route path="staff/member" element={<MyManageUser />} />
                     <Route path="staff/member/newStudents" element={<MyManageNewUser />} />
+                    <Route path="staff/member/students" element={<MyManageExistUser />} />
                 </Routes>
             </FlexDiv>
         </>
