c3e5e80543318e7cf197668ab032ccafdc5c8a3f
Yejin0O0 <yyj11kr@naver.com>
2024-02-26 23:46:37 +0900

    [Fix/#169] fix: fetch 조건 추가

--- src/Components/Common/HeaderNav.tsx
+++ src/Components/Common/HeaderNav.tsx
@@ -87,7 +87,9 @@ const HeaderNav = () => {
 
     // signup이 안된 경우 profile은 null 로, accessToken은 default로 초기화해주어야 함.
     useEffect(() => {
-        fetchSigningUserData("/signUp/check", "GET", "token");
+        if (access !== "default") {
+            fetchSigningUserData("/signUp/check", "GET", "token");
+        }
     }, []);
 
     useEffect(() => {
