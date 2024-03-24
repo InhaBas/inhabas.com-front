3657d3c3e2491fbc47c94b474a0681dc13dc2ef7
JiSeong Kim <12180543@inha.edu>
2024-03-24 22:39:39 +0900

    [feat/#229] feat: BankTable, Bank 파일 수정
    - BankTable에 회계내역 정보 매핑
    - Bank에 페이지네이션, history 정보 fetch 적용

--- src/Components/Component/IBAS/Bank/BankTable.tsx
+++ src/Components/Component/IBAS/Bank/BankTable.tsx
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 import { theme } from "../../../../styles/theme";
 
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 import { modalInfo, modalOpen } from "../../../../Recoil/frontState";
 
@@ -11,80 +11,52 @@ import { Div, FlexDiv } from "../../../../styles/assets/Div";
 import Img from "../../../../styles/assets/Img";
 import P from "../../../../styles/assets/P";
 
+import { bankHistoryInfo, bankBalanceInfo } from "../../../../Recoil/backState";
+
+import Modal from "../../../Common/Modal/ModalHistory";
+import useFetch from "../../../../Hooks/useFetch";
+
 const BankTable = () => {
     const navigate = useNavigate();
 
     const setOpen = useSetRecoilState(modalOpen);
     const setModalInfo = useSetRecoilState(modalInfo);
 
-    const clickEvent = (ev: MouseEvent, name: string) => {
+    const [_delete, fetchDeleteHistory] = useFetch();
+
+    const clickDetailEvent = (ev: MouseEvent, name: string, selectedId: number) => {
         setOpen(true);
         console.log("gg");
 
-        setModalInfo({ type: "bank" });
+        setModalInfo({ type: "bankHistory", content: String(selectedId) });
+    };
+
+    const clickUpdateEvent = (ev: MouseEvent, name: string, selectedIdx: number) => {
+        setOpen(true);
+        console.log("gg");
+        console.log(selectedIdx)
+
+        setModalInfo({ type: "bankHistoryUpdate", content: String(selectedIdx) });
+    };
+
+    const clickDeleteEvent = (ev: MouseEvent, name: string, selectedId: number) => {
+        if (window.confirm('정말로 삭제하시겠습니까?')) {
+            fetchDeleteHistory(`/budget/history/${selectedId}`, 'DELETE', 'token')
+        }
+    };
+
+    const clickPostEvent = (ev: MouseEvent, name: string, content: string) => {
+        setOpen(true);
+        console.log("gg");
+
+        setModalInfo({ type: "bankHistoryPost" });
     };
 
     const headerInfo = ["사용일", "게시일", "수정일", "내용", "수입액", "지출액", "증빙"];
-    const widthList = [150, 150, 150, 400, 120, 120, 90];
+    const widthList = [90, 90, 90, 350, 100, 100, 40];
 
-    const contents = [
-        {
-            사용일: "2023-01-02",
-            게시일: "2023-01-03",
-            수정일: "2023-01-04",
-            내용: "2023-01-05",
-            수입액: "0",
-            지출액: "60,000",
-        },
-        {
-            사용일: "2023-01-02",
-            게시일: "2023-01-03",
-            수정일: "2023-01-04",
-            내용: "2023-01-05",
-            수입액: "0",
-            지출액: "60,000",
-        },
-        {
-            사용일: "2023-01-02",
-            게시일: "2023-01-03",
-            수정일: "2023-01-04",
-            내용: "2023-01-05",
-            수입액: "0",
-            지출액: "60,000",
-        },
-        {
-            사용일: "2023-01-02",
-            게시일: "2023-01-03",
-            수정일: "2023-01-04",
-            내용: "2023-01-05",
-            수입액: "0",
-            지출액: "60,000",
-        },
-        {
-            사용일: "2023-01-02",
-            게시일: "2023-01-03",
-            수정일: "2023-01-04",
-            내용: "2023-01-05",
-            수입액: "0",
-            지출액: "60,000",
-        },
-        {
-            사용일: "2023-01-02",
-            게시일: "2023-01-03",
-            수정일: "2023-01-04",
-            내용: "2023-01-05",
-            수입액: "0",
-            지출액: "60,000",
-        },
-        {
-            사용일: "2023-01-02",
-            게시일: "2023-01-03",
-            수정일: "2023-01-04",
-            내용: "2023-01-05",
-            수입액: "0",
-            지출액: "60,000",
-        },
-    ];
+    const contents = useRecoilValue(bankHistoryInfo);
+    const bankBalance = useRecoilValue(bankBalanceInfo);
 
     return (
         <>
@@ -103,8 +75,14 @@ const BankTable = () => {
                             </P>
                         </FlexDiv>
                     ))}
+                    {/* 권한 여부 체크 */}
+                    <FlexDiv $minWidth="60px" $padding="10px">
+                        <P $center fontWeight={700}>
+                            비고
+                        </P>
+                    </FlexDiv>
                 </FlexDiv>
-                {contents.map((element: object, idx: number) => (
+                {contents?.map((element: any, idx: number) => (
                     <FlexDiv
                         key={`contentItem${idx}`}
                         width="100%"
@@ -113,24 +91,38 @@ const BankTable = () => {
                         $justifycontent="space-between"
                         $backgroundColor="wh"
                     >
-                        {Object.values(element).map((item: any, idx: number) => (
+                        {Object.values(element).slice(1).map((item: any, idx: number) => (
                             <FlexDiv key={`itemValue${idx}`} $minWidth={`${widthList[idx]}px`} $padding="10px">
                                 <P $center fontWeight={500}>
                                     {item === "0" ? "-" : item}
                                 </P>
                             </FlexDiv>
                         ))}
-                        <FlexDiv width="90px">
-                            <Div width="15px" onClick={(e: MouseEvent) => clickEvent(e, "ss")} $pointer>
-                                <Img src="/images/file.svg" />
+                        <FlexDiv width="50px">
+                            <Div width="15px" onClick={ (e: MouseEvent) => clickDetailEvent(e, "ss", element?.id) } $pointer>
+                                <Img src="/images/file_grey.svg" />
                             </Div>
+                        </FlexDiv>
+                        {/* 권한 여부 체크 */}
+                        <FlexDiv $minWidth={'60px'} $padding="10px">
+                            <FlexDiv width="15px" $margin="0 6px" onClick={ (e: MouseEvent) => clickUpdateEvent(e, "ss", element?.id) } $pointer>
+                                <Img src="/images/pencil_grey.svg"/>
+                            </FlexDiv>
+                            <FlexDiv width="15px" onClick={ (e: MouseEvent) => clickDeleteEvent(e, "ss", element?.id) } $pointer>
+                                <Img src="/images/trash_grey.svg"/>
+                            </FlexDiv>
                         </FlexDiv>
                     </FlexDiv>
                 ))}
             </Div>
+            <FlexDiv width="100%" $justifycontent="flex-end" $padding="0 20px 10px 0" $margin="0 20px 0 0">
+                <FlexDiv width="20px" height="20px" radius={50} onClick={ (e: MouseEvent) => clickPostEvent(e, "ss", '') } $pointer>
+                    <Img src="/images/plus_grey.svg" />
+                </FlexDiv>
+            </FlexDiv>
             <FlexDiv width="100%">
                 <Div $padding="10px 20px" $backgroundColor="bgColor" radius={50}>
-                    <P color="wh"> 잔액 : ₩ 10,000 </P>
+                    <P color="wh"> 잔액 : ₩ { String(bankBalance)?.replace(/\B(?=(\d{3})+(?!\d))/g, ',') } </P>
                 </Div>
             </FlexDiv>
         </>
--- src/Components/Page/IBAS/Bank/Bank.tsx
+++ src/Components/Page/IBAS/Bank/Bank.tsx
@@ -3,43 +3,135 @@ import { H2 } from "../../../../styles/assets/H";
 import { Select } from "../../../../styles/assets/Input";
 
 import BankTable from "../../../Component/IBAS/Bank/BankTable";
+import Pagination from "../../../Common/Pagination";
+
+import useFetch from "../../../../Hooks/useFetch";
+
+import { bankHistoryInfo, totalPageInfo, bankBalanceInfo, bankYearsInfo } from "../../../../Recoil/backState";
+import { useRecoilState } from "recoil";
+import { useEffect, useState } from "react";
+
+import { modalInfo } from "../../../../Recoil/frontState";
+
+export interface bankHistoryInterface {
+    id?: number;
+    dateUsed: string;
+    dateCreated: string;
+    dateUpdated: string;
+    title: string;
+    income: string;
+    outcome: string;
+    memberStudentIdReceived: string;
+    memberNameReceived: string;
+    memberStudentIdInCharge: string;
+    memberNameInCharge: string;
+}
 
 const Bank = () => {
+
+    const [nowModalInfo, setNowModalInfo] = useRecoilState(modalInfo);
+    
+    const [bankHistoryData, fetchBankHistoryData] = useFetch();
+    const [bankHistory, setBankHistory] = useRecoilState(bankHistoryInfo);
+    const [bankBalance, setBankBalance] = useRecoilState(bankBalanceInfo);
+    const [totalPage, setTotalPage] = useRecoilState(totalPageInfo);
+
+    const [bankYearsData, fetchBankYearsData] = useFetch();
+    const [bankYears, setBankYears] = useRecoilState(bankYearsInfo);
+
+    const [selectedYear, setSelectedYear] = useState('');
+
+    const handleSelectedYear = (e: any) => {
+        setSelectedYear(e.target.value);
+    }
+
+    // 연도 데이터 패치
+    useEffect(() => {
+        fetchBankYearsData('/budget/histories/years', 'GET');
+    }, [])
+
+    // 연도 데이터 패치 후 bankYears에 매핑
+    useEffect(() => {
+        if (bankYearsData) {
+            setBankYears(bankYearsData)
+        }
+    }, [bankYearsData])
+
+    // bankYears에 연도 데이터 매핑 후, 전체 히스토리 패치
+    useEffect(() => {
+        if (bankYears && bankYears[0]) {
+            fetchBankHistoryData(`/budget/histories?year=${selectedYear}&page=${'0'}&size=${'15'}`, "GET", "token")
+        }
+    }, [bankYears, selectedYear])
+
+    useEffect(() => {
+        if (bankHistoryData) {
+            const contents = bankHistoryData?.page?.data?.map((data : any) => ({
+                id: data?.id,
+                사용일: data?.dateUsed?.split('T')[0].substr(2),
+                게시일: data?.dateCreated?.split('T')[0].substr(2),
+                수정일: data?.dateUpdated?.split('T')[0].substr(2),
+                내용: data?.title,
+                수입액: String(data?.income)?.replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+                지출액: String(data?.outcome)?.replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+            }))
+            setBankHistory(contents);
+        }
+
+        setTotalPage(bankHistoryData?.page?.pageInfo?.totalPages);
+        setBankBalance(bankHistoryData?.balance)
+    }, [bankHistoryData])
+
     return (
         <FlexDiv width="100%">
-            <FlexDiv width="80%">
-                <FlexDiv
-                    $border="3px solid"
-                    $borderColor="border"
-                    width="98%"
-                    $justifycontent="space-between"
-                    $padding="20px"
-                    $margin="0 0 50px 0"
-                >
-                    <Div>
-                        <H2 fontSize="xl" fontWeight={700}>
-                            전체 회계 내역
-                        </H2>
-                    </Div>
-                    <Div>
-                        <Select
-                            name="subject"
-                            required
-                            width="100px"
-                            $backgroundColor="bgColor"
-                            $borderRadius={100}
-                            color="wh"
-                            $padding="10px"
+            <FlexDiv $padding="80px">
+                <FlexDiv width="80%">
+                    <FlexDiv
+                        $border="3px solid"
+                        $borderColor="border"
+                        width="98%"
+                        $justifycontent="space-between"
+                        $padding="20px"
+                        $margin="0 0 50px 0"
                         >
-                            <option hidden>전체보기</option>
-                            <option>2022</option>
-                            <option>2023</option>
-                        </Select>
-                    </Div>
-                </FlexDiv>
-                <BankTable />
+                        <Div>
+                            <H2 fontSize="xl" fontWeight={700}>
+                                전체 회계 내역
+                            </H2>
+                        </Div>
+                        <Div>
+                            <Select
+                                name="subject"
+                                required
+                                width="100px"
+                                $backgroundColor="bgColor"
+                                $borderRadius={100}
+                                color="wh"
+                                $padding="10px"
+                                onChange={handleSelectedYear}
+                                value={selectedYear}
+                                >
+                                <option hidden>전체보기</option>
+                                {Object.values(bankYears).sort((a, b) => b-a).map((bankYear => (
+                                    <option value={String(bankYear)}>{bankYear}</option>
+                                )))}
+                            </Select>
+                        </Div>
+                    </FlexDiv>
+                    <BankTable />
 
-                {/* <Pagination /> */}
+                    {/* <Pagination /> */}
+                    {bankHistory && bankHistory.length !== 0 && (
+                        <Pagination
+                            totalPage={totalPage}
+                            fetchUrl={`/budget/histories`}
+                            token
+                            paginationFetch={fetchBankHistoryData}
+                            size={15}
+                            search={`&year=${selectedYear}`}
+                        />
+                    )}
+                </FlexDiv>
             </FlexDiv>
         </FlexDiv>
     );
