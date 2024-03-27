ea044a2ea1cd6fb5aedca5a977663b59b0cf471a
Yejin0O0 <yyj11kr@naver.com>
2024-03-27 23:14:34 +0900

    [feat/#288] alert 추가

--- src/Routes/MainRoute.tsx
+++ src/Routes/MainRoute.tsx
@@ -1,4 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+import { GetRoleAuthorization } from "../Functions/authFunctions";
+import { userRole } from "../Recoil/backState";
+
 import Activity from "../Components/Page/IBAS/Activity/Activity";
 import ActivityCreate from "../Components/Page/IBAS/Activity/ActivityCreate";
 import ActivityDetail from "../Components/Page/IBAS/Activity/ActivityDetail";
@@ -6,10 +12,23 @@ import Bank from "../Components/Page/IBAS/Bank/Bank";
 import BankSupport from "../Components/Page/IBAS/BankSupport/BankSupport";
 import BankSupportCreate from "../Components/Page/IBAS/BankSupport/BankSupportCreate";
 import BankSupportDetail from "../Components/Page/IBAS/BankSupport/BankSupportDetail";
-import { GetRoleAuthorization } from "../Functions/authFunctions";
 
 const MainRoute = () => {
+    const navigate = useNavigate();
     const { isAuthorizedOverBasic } = GetRoleAuthorization();
+    const role = useRecoilValue(userRole);
+
+    useEffect(() => {
+        if (role !== "") {
+            if (!isAuthorizedOverBasic) {
+                alert("권한이 없습니다");
+                setTimeout(() => {
+                    navigate(-1); // 이전 페이지로 돌아감
+                }, 0);
+            }
+        }
+    }, [role]);
+
     return (
         <Routes>
             <Route path="activity" element={<Activity />} />
