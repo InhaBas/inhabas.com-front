e9d6f45211cacd2ed8a20aacfb2d0372cfcfc355
Yejin0O0 <yyj11kr@naver.com>
2024-03-29 10:09:41 +0900

    [feat/#305] feat: url 변경될 때 마다 scroll 위치 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,17 @@ import { tokenAccess } from "./Recoil/backState";
 import Bottom from "./Components/Common/Bottom";
 import Carousel from "./Components/Common/Carousel";
 import { Modal } from "./Components/Common/Modal/Modal";
+import NotFound from "./Components/Page/Error/NotFound";
 import Login from "./Components/Page/Member/Login";
 import LoginProcess from "./Components/Page/Member/LoginProcess";
 import Rule from "./Components/Page/Member/Rule";
 import Signup from "./Components/Page/Member/Signup";
 import SignupQuestion from "./Components/Page/Member/SignupQuestion";
 import HeaderNavLayout from "./Layout/HeaderNavLayout";
-import NotFound from "./Components/Page/Error/NotFound";
 
 import { Div } from "./styles/assets/Div";
 
+import ScrollToTop from "./Components/Common/ScrollToTop";
 import "./index.css";
 
 const App = () => {
@@ -34,6 +35,7 @@ const App = () => {
             <GlobalStyle />
             <Modal />
             <Div width="100%">
+                <ScrollToTop />
                 <Routes>
                     {/* HeaderNav가 필요 없는 page 일 경우 여기에 Route 정의  */}
                     <Route path="slide" element={<Carousel />} />
