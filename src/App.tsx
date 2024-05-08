849ec0b3624fe046c2b375bae1aeb159a5de3429
Yejin0O0 <yyj11kr@naver.com>
2024-05-08 11:07:48 +0900

    [fix/#405] fix: url 직접 입력해서 로그인 권한 필요한 페이지 들어가는 것 방지

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 import { ThemeProvider } from "styled-components";
@@ -6,10 +7,11 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/Globalstyles";
 import { theme } from "./styles/theme";
 
-import { tokenAccess } from "./Recoil/backState";
+import { failRefreshing } from "./Recoil/frontState";
 
 import Bottom from "./Components/Common/Bottom";
 import { Modal } from "./Components/Common/Modal/Modal";
+import ScrollToTop from "./Components/Common/ScrollToTop";
 import NotFound from "./Components/Page/Error/NotFound";
 import Login from "./Components/Page/Member/Login";
 import LoginProcess from "./Components/Page/Member/LoginProcess";
@@ -20,11 +22,26 @@ import HeaderNavLayout from "./Layout/HeaderNavLayout";
 
 import { Div } from "./styles/assets/Div";
 
-import ScrollToTop from "./Components/Common/ScrollToTop";
 import "./index.css";
 
 const App = () => {
-    const access = useRecoilValue(tokenAccess);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const isNotLogin = useRecoilValue(failRefreshing);
+
+    useEffect(() => {
+        // 페이지 경로에 따라 로그인 상태를 검사하고 알림을 표시합니다.
+
+        if (
+            (isNotLogin && ["/honor"]?.includes(location.pathname)) ||
+            (isNotLogin &&
+                location.pathname.substring(1) &&
+                !["opensource", "sponsor", "usage", "contest", "activity"]?.includes(location.pathname))
+        ) {
+            alert("로그인을 해주세요");
+            navigate("/");
+        }
+    }, [location, isNotLogin]);
 
     return (
         <ThemeProvider theme={theme}>
