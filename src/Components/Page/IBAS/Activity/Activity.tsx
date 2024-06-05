64ff658d8d82bac666859727ff69cb0e15f2e2ea
Yejin0O0 <yyj11kr@naver.com>
2024-06-05 10:26:57 +0900

    [feat/#472] activity loading 추가

--- src/Components/Page/IBAS/Activity/Activity.tsx
+++ src/Components/Page/IBAS/Activity/Activity.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
 
 import useFetch from "../../../../Hooks/useFetch";
 
-import Button from "../../../../styles/assets/Button";
-import { Div, FlexDiv } from "../../../../styles/assets/Div";
-import Img from "../../../../styles/assets/Img";
-import P from "../../../../styles/assets/P";
-
-import Pagination from "../../../Common/Pagination";
-
-import { useRecoilState } from "recoil";
 import { totalPageInfo } from "../../../../Recoil/backState";
 
 import { GetRoleAuthorization } from "../../../../Functions/authFunctions";
 
 import { ActivityInterface } from "../../../../Types/IBAS/TypeIBAS";
+import Button from "../../../../styles/assets/Button";
+import { Div, FlexDiv } from "../../../../styles/assets/Div";
+import Img from "../../../../styles/assets/Img";
+import P from "../../../../styles/assets/P";
+
+import Loading from "../../../Common/Loading";
+import Pagination from "../../../Common/Pagination";
 import ActivityCard from "../../../Component/IBAS/Activity/ActivityCard";
 
 const Activity = () => {
@@ -24,8 +24,10 @@ const Activity = () => {
     const [activityListData, fetchActivityListData] = useFetch();
     const [detail, setDetail] = useState<ActivityInterface[] | null>(null);
     const { isAuthorizedOverSecretary } = GetRoleAuthorization();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        setIsLoading(true);
         fetchActivityListData("/club/activities?page=0&size=6", "GET", "token");
     }, []);
 
@@ -33,15 +35,25 @@ const Activity = () => {
         if (activityListData) {
             setDetail(activityListData.data);
             setTotalPage(activityListData.pageInfo.totalPages);
+            setIsLoading(false);
         }
+        return () => {
+            setDetail(null);
+            setTotalPage(0);
+        };
     }, [activityListData]);
 
     const moveCreate = () => navigate("/activity/create");
 
     return (
         <>
-            <FlexDiv width="100%" $justifycontent="space-around" direction="column">
-                {/* <FlexDiv width="70%" $maxWidth="1500px" $justifycontent="center" $margin="50px 0 0 0">
+            {isLoading ? (
+                <FlexDiv width="100%" height="100vh">
+                    <Loading />
+                </FlexDiv>
+            ) : (
+                <FlexDiv width="100%" $justifycontent="space-around" direction="column">
+                    {/* <FlexDiv width="70%" $maxWidth="1500px" $justifycontent="center" $margin="50px 0 0 0">
                     {detail?.slice(0, 3)?.map(({ thumbnail, title, dateCreated, writerName, id }) => (
                         <FlexDiv $margin="50px 25px">
                             <ActivityCard
@@ -91,66 +103,67 @@ const Activity = () => {
                     )}
                 </FlexDiv> */}
 
-                <FlexDiv
-                    width="70%"
-                    $maxWidth="1500px"
-                    $justifycontent="center"
-                    $alignitems="start"
-                    $margin="50px 0 0 0"
-                >
-                    {detail?.map(({ thumbnail, title, dateCreated, writerName, id }) => (
-                        <Div width="360px" $margin="0 25px 50px 25px">
-                            <ActivityCard
-                                imgSrc={thumbnail?.url}
-                                title={title}
-                                dateCreated={dateCreated}
-                                writerName={writerName}
-                                id={id}
-                            />
-                        </Div>
-                    ))}
-                    {detail?.length === 2 || detail?.length === 5 ? (
-                        <Div width="360px" height="360px" $margin="0 25px 50px 25px" />
-                    ) : detail?.length === 1 || detail?.length === 4 ? (
-                        <>
+                    <FlexDiv
+                        width="70%"
+                        $maxWidth="1500px"
+                        $justifycontent="center"
+                        $alignitems="start"
+                        $margin="50px 0 0 0"
+                    >
+                        {detail?.map(({ thumbnail, title, dateCreated, writerName, id }) => (
+                            <Div width="360px" $margin="0 25px 50px 25px">
+                                <ActivityCard
+                                    imgSrc={thumbnail?.url}
+                                    title={title}
+                                    dateCreated={dateCreated}
+                                    writerName={writerName}
+                                    id={id}
+                                />
+                            </Div>
+                        ))}
+                        {detail?.length === 2 || detail?.length === 5 ? (
                             <Div width="360px" height="360px" $margin="0 25px 50px 25px" />
+                        ) : detail?.length === 1 || detail?.length === 4 ? (
+                            <>
+                                <Div width="360px" height="360px" $margin="0 25px 50px 25px" />
 
-                            <Div width="360px" height="360px" $margin="0 25px 50px 25px" />
-                        </>
-                    ) : (
-                        ""
+                                <Div width="360px" height="360px" $margin="0 25px 50px 25px" />
+                            </>
+                        ) : (
+                            ""
+                        )}
+                    </FlexDiv>
+                    <Div width="70%" $margin="0 0 50px 0">
+                        <Pagination
+                            totalPage={totalPage}
+                            fetchUrl="/club/activities"
+                            paginationFetch={fetchActivityListData}
+                            size={6}
+                        />
+                    </Div>
+                    {isAuthorizedOverSecretary && (
+                        <FlexDiv $margin="50px 0" width="70%" $justifycontent="end">
+                            <Button
+                                display="flex"
+                                $backgroundColor="bgColor"
+                                $padding="12px 15px"
+                                $borderRadius={30}
+                                $HBackgroundColor="bgColorHo"
+                                onClick={() => moveCreate()}
+                            >
+                                <Div width="12px" $margin="0 10px 0 0">
+                                    <Img src="/images/plus_white.svg" />
+                                </Div>
+                                <Div $pointer>
+                                    <P color="wh" fontSize="sm">
+                                        게시글 등록
+                                    </P>
+                                </Div>
+                            </Button>
+                        </FlexDiv>
                     )}
                 </FlexDiv>
-                <Div width="70%" $margin="0 0 50px 0">
-                    <Pagination
-                        totalPage={totalPage}
-                        fetchUrl="/club/activities"
-                        paginationFetch={fetchActivityListData}
-                        size={6}
-                    />
-                </Div>
-                {isAuthorizedOverSecretary && (
-                    <FlexDiv $margin="50px 0" width="70%" $justifycontent="end">
-                        <Button
-                            display="flex"
-                            $backgroundColor="bgColor"
-                            $padding="12px 15px"
-                            $borderRadius={30}
-                            $HBackgroundColor="bgColorHo"
-                            onClick={() => moveCreate()}
-                        >
-                            <Div width="12px" $margin="0 10px 0 0">
-                                <Img src="/images/plus_white.svg" />
-                            </Div>
-                            <Div $pointer>
-                                <P color="wh" fontSize="sm">
-                                    게시글 등록
-                                </P>
-                            </Div>
-                        </Button>
-                    </FlexDiv>
-                )}
-            </FlexDiv>
+            )}
         </>
     );
 };
