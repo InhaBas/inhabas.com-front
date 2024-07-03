204d538294537556de4afdcf7f11d7fcf83933af
Yejin0O0 <yyj11kr@naver.com>
2024-07-04 00:28:38 +0900

    [fix/#544] refactor: carousel 최적화

--- src/Components/Common/Carousel.tsx
+++ src/Components/Common/Carousel.tsx
@@ -1,8 +1,7 @@
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-import Slider from "react-slick";
-
+import Slider, { Settings } from "react-slick";
 import styled from "styled-components";
 
 import { useEffect, useRef, useState } from "react";
@@ -18,56 +17,48 @@ const StyledSlider = styled(Slider)`
     height: calc(100vh - 200px);
 `;
 
-const NextArrow = (props: any) => {
-    const { onClick } = props;
-    return (
-        <Div
-            $position="absolute"
-            $right="10px"
-            $top="50%"
-            $zIndex={2}
-            $backgroundColor="bgColor"
-            $padding="10px"
-            $pointer
-            onClick={onClick}
-        >
-            <Div width="22px" height="22px">
-                <Img src="/images/arrow-right_white.svg" />
-            </Div>
-        </Div>
-    );
-};
+interface ArrowProps {
+    onClick?: () => void;
+}
 
-const PrevArrow = (props: any) => {
-    const { onClick } = props;
-    return (
-        <Div
-            $position="absolute"
-            $left="10px"
-            $top="50%"
-            $zIndex={2}
-            $backgroundColor="bgColor"
-            $padding="10px"
-            $pointer
-            onClick={onClick}
-        >
-            <Div width="22px" height="22px">
-                <Img src="/images/arrow-left_white.svg" />
-            </Div>
+const ArrowButton = styled(Div)<{ $left?: string; $right?: string }>`
+    position: absolute;
+    top: 50%;
+    z-index: 2;
+    background-color: #4611a7;
+    padding: 10px;
+    cursor: pointer;
+    ${({ $left }) => $left && `left: ${$left};`}
+    ${({ $right }) => $right && `right: ${$right};`}
+`;
+
+const NextArrow: React.FC<ArrowProps> = ({ onClick }) => (
+    <ArrowButton $right="10px" onClick={onClick}>
+        <Div width="22px" height="22px">
+            <Img src="/images/arrow-right_white.svg" />
         </Div>
-    );
-};
+    </ArrowButton>
+);
+
+const PrevArrow: React.FC<ArrowProps> = ({ onClick }) => (
+    <ArrowButton $left="10px" onClick={onClick}>
+        <Div width="22px" height="22px">
+            <Img src="/images/arrow-left_white.svg" />
+        </Div>
+    </ArrowButton>
+);
 
 const Carousel: React.FC<carouselInterface> = ({ images }) => {
-    const sliderRef = useRef<any>(null);
+    const sliderRef = useRef<Slider>(null);
     const setIsCarouselOpen = useSetRecoilState(carouselOpen);
     const carouselInitial = useRecoilValue(carouselInitialState);
-    const [currentSlide, setCurrentSlide] = useState(carouselInitial);
+    const [currentSlide, setCurrentSlide] = useState<number>(carouselInitial);
+
     const moveBack = () => setIsCarouselOpen(false);
 
-    const settings = {
-        customPaging: (i: number) => {
-            const thumb = images[i]; // images 배열에서 썸네일 이미지 경로를 가져옵니다.
+    const settings: Settings = {
+        customPaging: (i) => {
+            const thumb = images[i];
             return (
                 <Div display="inline-block" height="100px" width="100px" $margin="0 10px 0 0" $pointer>
                     <Img $objectFit="fill" src={thumb} />
@@ -78,21 +69,19 @@ const Carousel: React.FC<carouselInterface> = ({ images }) => {
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow: <PrevArrow />, // 이전 화살표 모양 설정
-        nextArrow: <NextArrow />, // 다음 화살표 모양 설정
+        prevArrow: <PrevArrow />,
+        nextArrow: <NextArrow />,
         swipeToSlide: true,
         initialSlide: carouselInitial,
-        afterChange: (index: number) => setCurrentSlide(index),
+        afterChange: (index) => setCurrentSlide(index),
     };
 
-    // 이미지 다운로드 함수
     const handleDownload = async () => {
         const response = await fetch(images[currentSlide]);
         const file = await response.blob();
         const downloadUrl = window.URL.createObjectURL(file);
 
         const link = document.createElement("a");
-        // link.href = images[currentSlide];
         link.href = downloadUrl;
         link.download = `image_${currentSlide + 1}.jpg`;
         document.body.appendChild(link);
@@ -101,20 +90,11 @@ const Carousel: React.FC<carouselInterface> = ({ images }) => {
     };
 
     useEffect(() => {
-        if (sliderRef.current) {
-            const slider = sliderRef.current;
-            if (slider.innerSlider) {
-                const currentSlide = slider.innerSlider.list.querySelector(".slick-current");
-                if (currentSlide) {
-                    currentSlide.classList.remove("slick-current");
-                    currentSlide.classList.add("slick-center");
-                }
-            }
-        }
+        setCurrentSlide(carouselInitial);
         return () => {
             setCurrentSlide(0);
         };
-    }, [images]);
+    }, [images, carouselInitial]);
 
     return (
         <Div width="100%" height="100vh" $position="absolute" $top="0" $left="0" $zIndex={9999} $backgroundColor="wh">
@@ -126,7 +106,7 @@ const Carousel: React.FC<carouselInterface> = ({ images }) => {
                         </P>
                     </Div>
                     <FlexDiv width="100px" $justifycontent="space-around">
-                        <Div $pointer width="20px" height="20px" onClick={() => handleDownload()}>
+                        <Div $pointer width="20px" height="20px" onClick={handleDownload}>
                             <Img src="/images/download_white.svg" />
                         </Div>
                         <Div $pointer $margin="0 30px 0 0" onClick={moveBack}>
@@ -136,19 +116,14 @@ const Carousel: React.FC<carouselInterface> = ({ images }) => {
                         </Div>
                     </FlexDiv>
                 </FlexDiv>
-                {/* 이미지가 하나인 경우 슬라이더 대신 단일 이미지를 렌더링합니다. */}
                 {images.length === 1 ? (
                     <FlexDiv width="100%" height="calc(100vh - 50px)">
                         <Img $objectFit="contain" src={images[0]} />
                     </FlexDiv>
                 ) : (
                     <StyledSlider {...settings} ref={sliderRef}>
-                        {images.map((image: string, index: number) => (
-                            <Div
-                                key={`image${index}`}
-                                height="calc(100vh - 200px)"
-                                style={{ display: index === currentSlide ? "block" : "none" }}
-                            >
+                        {images.map((image, index) => (
+                            <Div key={index} height="calc(100vh - 200px)">
                                 <Img $objectFit="contain" src={image} />
                             </Div>
                         ))}
