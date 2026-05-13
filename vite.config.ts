b59b1e99b7ac2c8c8939f11e6ba11d6b16088837
haewonwon <khcw1029@daum.net>
2026-05-13 15:24:59 +0900

    chore: [WIP] vite.config.ts 추가

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [react()],
+    optimizeDeps: {
+        include: [
+            "@toast-ui/react-editor",
+            "@toast-ui/editor",
+            "@toast-ui/editor-plugin-code-syntax-highlight",
+            "@toast-ui/editor-plugin-color-syntax",
+        ],
+    },
+    server: {
+        port: 3000,
+        open: true,
+    },
+    build: {
+        outDir: "build",
+    },
+});
