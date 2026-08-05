a00c514ecee71d46f8eeede75508616d4c8f5689
haewonwon <khcw1029@daum.net>
2026-08-06 04:01:17 +0900

    fix: SeeMoreButton 반응형 레이아웃 수정

--- src/components/scholarship/SeeMoreButton.tsx
+++ src/components/scholarship/SeeMoreButton.tsx
@@ -1,7 +1,22 @@
 import { Div, FlexDiv } from "../../styles/assets/Div";
 import P from "../../styles/assets/P";
+import styled from "styled-components";
 
 import { useNavigate } from "react-router-dom";
+
+const MoreButton = styled(FlexDiv)`
+    max-width: 100%;
+`;
+
+const MoreText = styled(P)`
+    white-space: normal;
+    overflow: visible;
+    text-overflow: clip;
+`;
+
+const Arrow = styled(Div)`
+    flex-shrink: 0;
+`;
 
 const SeeMoreButton = ({ url }: {url: string}) => {
 
@@ -13,12 +28,12 @@ const SeeMoreButton = ({ url }: {url: string}) => {
 
     return (
         <>
-            <FlexDiv onClick={clickBtnEvent}>
+            <MoreButton onClick={clickBtnEvent}>
                 <Div $margin="0 5px 0 0">
-                    <P color="grey">더보기</P>
+                    <MoreText color="grey">더보기</MoreText>
                 </Div>
-                <Div $borderL="14px solid grey" $borderT="7px solid transparent" $borderB="7px solid transparent" />
-            </FlexDiv>
+                <Arrow $borderL="14px solid grey" $borderT="7px solid transparent" $borderB="7px solid transparent" />
+            </MoreButton>
         </>
     )
 }
