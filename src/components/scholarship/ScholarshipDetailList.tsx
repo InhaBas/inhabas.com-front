7b4d49f9f722840a696067cf8d6ff5f1173c2b83
haewonwon <khcw1029@daum.net>
2026-08-06 04:01:13 +0900

    fix: ScholarshipDetailList 반응형 레이아웃 수정

--- src/components/scholarship/ScholarshipDetailList.tsx
+++ src/components/scholarship/ScholarshipDetailList.tsx
@@ -1,10 +1,35 @@
 import { Div, FlexDiv } from "../../styles/assets/Div";
 import P from "../../styles/assets/P";
 import Button from "../../styles/assets/Button";
+import styled from "styled-components";
+import { media } from "../../styles/theme";
 
 import { scholarshipDetailListInterface } from "../../types/ibas/TypeIBAS";
 
 import { useNavigate } from "react-router-dom";
+
+const DetailItem = styled(Div)`
+    box-sizing: border-box;
+    max-width: 100%;
+`;
+
+const DetailButton = styled(Button)`
+    max-width: 100%;
+
+    ${media.tablet} {
+        text-align: left;
+    }
+`;
+
+const DetailContent = styled(P)`
+    ${media.tablet} {
+        white-space: normal;
+        overflow: visible;
+        text-overflow: clip;
+        overflow-wrap: anywhere;
+        line-height: 1.5;
+    }
+`;
 
 const ScholarshipDetailList: React.FC<scholarshipDetailListInterface> = ({contents, mainUrl}) => {
     const navigate = useNavigate()
@@ -13,9 +38,9 @@ const ScholarshipDetailList: React.FC<scholarshipDetailListInterface> = ({conten
         <>
             {
                 contents?.slice(0, 3)?.map(({ date, content, id }) => (
-                    <Div width="100%" $borderB="2px solid grey" $margin="0 0 15px 0" $padding="0 0 7px 0">
+                    <DetailItem width="100%" $borderB="2px solid grey" $margin="0 0 15px 0" $padding="0 0 7px 0">
                         {/* 링크 */}
-                        <Button width="100%" onClick={() => navigate(`${mainUrl}/${id}`)}>
+                        <DetailButton width="100%" onClick={() => navigate(`${mainUrl}/${id}`)}>
                             <Div>
                                 {/* 날짜 */}
                                 <FlexDiv $backgroundColor="grey" radius={20} width="50px" height="18px" $margin="0 0 10px 0">
@@ -26,12 +51,12 @@ const ScholarshipDetailList: React.FC<scholarshipDetailListInterface> = ({conten
                             </Div>
                             <Div>
                                 {/* 내용 */}
-                                <P color="wh">
+                                <DetailContent color="wh">
                                     {content}
-                                </P>
+                                </DetailContent>
                             </Div>
-                        </Button>
-                    </Div>
+                        </DetailButton>
+                    </DetailItem>
                 ))
             }
         </>
